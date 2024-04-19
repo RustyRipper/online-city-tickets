0605d7be8df1bef4da7d7eac9a34055bfba1dcc3
Maciej Makara <maciejmakarakontakt@gmail.com>
2024-04-19 12:51:27 +0200

    init spring and angular

---
+++ online-city-tickets-backend/src/main/java/org/pwr/onlinecityticketsbackend/OnlineCityTicketsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.pwr.onlinecityticketsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCityTicketsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCityTicketsBackendApplication.class, args);
+	}
+
+}
---
+++ online-city-tickets-backend/src/test/java/org/pwr/onlinecityticketsbackend/OnlineCityTicketsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pwr.onlinecityticketsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCityTicketsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
