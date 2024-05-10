c37ebbd1ff9e7a589f28efec2aef81d94941a843
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-05-10 22:18:27 +0200

    feat: finish the login layout

--- online-city-tickets-frontend/src/app/auth/components/login/login.component.ts
+++ online-city-tickets-frontend/src/app/auth/components/login/login.component.ts
@@ -1,9 +1,41 @@
 import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { ButtonModule } from "primeng/button";
+import { InputTextModule } from "primeng/inputtext";
+import { PasswordModule } from "primeng/password";
+import { DividerModule } from "primeng/divider";
+
+import { TopBarComponent } from "../../../shared/components/top-bar/top-bar.component";
 
 @Component({
   selector: "app-login",
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    PasswordModule,
+    DividerModule,
+    TopBarComponent,
+  ],
   templateUrl: "./login.component.html",
+  styleUrl: "./login.component.css",
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected readonly form;
+
+  public constructor(formBuilder: FormBuilder) {
+    this.form = formBuilder.nonNullable.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  protected onSubmit() {
+    console.log(this.form.value);
+  }
+}
