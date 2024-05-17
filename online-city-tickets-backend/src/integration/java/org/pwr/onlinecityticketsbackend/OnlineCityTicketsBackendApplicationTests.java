8e2230237f9eb22652f159aa2110e6c8755544bf
Jakub Zehner <kubusja.pl@gmail.com>
2024-05-18 00:30:47 +0200

    add integration tests and check if fails properly

---
+++ online-city-tickets-backend/src/integration/java/org/pwr/onlinecityticketsbackend/OnlineCityTicketsBackendApplicationTests.java
@@ -0,0 +1,20 @@
+package org.pwr.onlinecityticketsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class OnlineCityTicketsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void simpleTest() {
+		assertEquals(4, 2 + 3);
+	}
+
+}
