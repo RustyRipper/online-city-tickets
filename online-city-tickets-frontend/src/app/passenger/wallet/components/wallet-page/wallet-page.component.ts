a21751139a6ebe9f68fdc830d063e15457c4cb05
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-05-17 20:17:10 +0200

    feat: add scaffoldings for all linked pages

--- online-city-tickets-frontend/src/app/passenger/wallet/components/wallet-page/wallet-page.component.ts
+++ online-city-tickets-frontend/src/app/passenger/wallet/components/wallet-page/wallet-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from "@angular/core";
 
+import { TopBarComponent } from "../../../../shared/components/top-bar/top-bar.component";
+import { BackButtonComponent } from "../../../../shared/components/back-button/back-button.component";
+
 @Component({
   selector: "app-wallet-page",
   standalone: true,
-  imports: [],
+  imports: [TopBarComponent, BackButtonComponent],
   templateUrl: "./wallet-page.component.html",
 })
 export class WalletPageComponent {}
