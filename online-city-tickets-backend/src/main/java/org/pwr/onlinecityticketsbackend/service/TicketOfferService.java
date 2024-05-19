0c3fa702f2116a5cb8444fe09ac7deb5bdb5e920
Jakub Zehner <kubusja.pl@gmail.com>
2024-05-19 19:38:09 +0200

    add note about bug

--- online-city-tickets-backend/src/main/java/org/pwr/onlinecityticketsbackend/service/TicketOfferService.java
+++ online-city-tickets-backend/src/main/java/org/pwr/onlinecityticketsbackend/service/TicketOfferService.java
@@ -22,6 +22,10 @@ public class TicketOfferService {
                 .toList();
     }
 
+    // NOTE: In this method we use existsById before findById due to the strange behavior of the findById method,
+    // which returns deleted entities, which should not be the case, this is probably a bug in Hibernate,
+    // as there is no information in the documentation that findById should return soft deleted entities.
+    // It is also possible that this bug only occurs in a test environment, which would require further testing.
     public Optional<BaseTicketOfferDto> getOfferById(long id) {
         return ticketOfferRepository.existsById(id)
                 ? ticketOfferRepository.findById(id).map(ticketOfferMapper::toDto)
