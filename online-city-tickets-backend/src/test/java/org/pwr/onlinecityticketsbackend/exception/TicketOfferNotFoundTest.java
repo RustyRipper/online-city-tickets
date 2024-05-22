ab0c9b35b6eb1ef903210d692b097121f9e68ef5
Jakub Zehner <kubusja.pl@gmail.com>
2024-05-22 13:30:10 +0200

    fix test

--- online-city-tickets-backend/src/test/java/org/pwr/onlinecityticketsbackend/exception/TicketOfferNotFoundTest.java
+++ online-city-tickets-backend/src/test/java/org/pwr/onlinecityticketsbackend/exception/TicketOfferNotFoundTest.java
@@ -2,6 +2,7 @@ package org.pwr.onlinecityticketsbackend.exception;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.pwr.onlinecityticketsbackend.exception.handler.RestApiException;
 import org.springframework.http.HttpStatus;
 
 public class TicketOfferNotFoundTest {
@@ -9,7 +10,7 @@ public class TicketOfferNotFoundTest {
 
     @Test
     public void shouldTicketOfferNotFoundBeCorrect() {
-        Assertions.assertThat(sut).isInstanceOf(Exception.class);
+        Assertions.assertThat(sut).isInstanceOf(RestApiException.class);
         Assertions.assertThat(sut.getHttpStatus()).isEqualTo(HttpStatus.NOT_FOUND);
         Assertions.assertThat(sut.getDescription()).isEqualTo("TICKET_OFFER_NOT_FOUND");
     }
