68eb7640c591a612a7ee2ab9a55d1b7a42c0e3aa
Tomasz Chojnacki <tomaszchojnacki2001@gmail.com>
2024-06-17 14:14:16 +0200

    feat: build the layout of card addition flow

--- online-city-tickets-frontend/src/app/passenger/credit-cards/components/card-addition-page/card-addition-page.component.ts
+++ online-city-tickets-frontend/src/app/passenger/credit-cards/components/card-addition-page/card-addition-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from "@angular/core";
+import { ButtonModule } from "primeng/button";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputMaskModule } from "primeng/inputmask";
+import { StepperModule } from "primeng/stepper";
 
 import { CardDisplayComponent } from "~/passenger/credit-cards/components/card-display/card-display.component";
 import { BackButtonComponent } from "~/shared/components/back-button/back-button.component";
@@ -8,7 +12,15 @@ import { I18nService } from "~/shared/i18n/i18n.service";
 @Component({
   selector: "app-card-addition-page",
   standalone: true,
-  imports: [TopBarComponent, BackButtonComponent, CardDisplayComponent],
+  imports: [
+    ButtonModule,
+    InputGroupModule,
+    InputMaskModule,
+    StepperModule,
+    TopBarComponent,
+    BackButtonComponent,
+    CardDisplayComponent,
+  ],
   templateUrl: "./card-addition-page.component.html",
   styleUrl: "./card-addition-page.component.css",
 })
